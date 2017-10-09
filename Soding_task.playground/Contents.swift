031604cf192fbf5414c9c9d2dace0ec5e3693f52
Kunal Gandhi <kunalgandhi@zzzs-MacBook-Air.local>
2017-10-09 20:35:52 +0530

    Task Done

---
+++ Soding_task.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+func calFibonacci(maxno:Int){
+    var result = 0
+    var num1 = 0;
+    var num2 = 1;
+    var tempnum = 0;
+   
+    repeat {
+        if (num2 % 2 == 0)
+        {
+            result += num2;
+        }
+        tempnum = num1 + num2;
+        num1 = num2;
+        num2 = tempnum;
+    } while (num2 < maxno);
+    
+    print(result);
+}
+
+calFibonacci(maxno: 4000000)
+
+
