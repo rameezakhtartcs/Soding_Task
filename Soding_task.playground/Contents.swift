b30de50b259758230c419052fb0109be49e6c4ce
Rameez <rameez.akhtar@tcs.com>
2018-01-09 01:04:55 +0530

    fixed 2621 issues

--- Soding_task.playground/Contents.swift
+++ Soding_task.playground/Contents.swift
@@ -24,7 +24,7 @@ func calFibonacci(maxno:Int){
 
 calFibonacci(maxno: 4000000)
 
-func addReplaceTD2621(){
+func addReplace2621(){
     
 }
 
@@ -34,6 +34,25 @@ func subtract(){
 
 func newmethod(){
     
+}
+
+func calprimeno(maxno:Int){
+    var result = 0
+    var num1 = 0;
+    var num2 = 1;
+    var tempnum = 0;
+    
+    repeat {
+        if (num2 % 2 == 0)
+        {
+            result += num2;
+        }
+        tempnum = num1 + num2;
+        num1 = num2;
+        num2 = tempnum;
+    } while (num2 < maxno);
+    
+    print(result);
 }
 
 
